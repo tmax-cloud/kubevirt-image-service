13507a482369a1b9607cb1ab770a5d962e7a0636
woohhan <woohyung_han@tmax.co.kr>
2020-04-23 10:57:04 +0900

    push docker image after build

--- e2e/main_test.go
+++ e2e/main_test.go
@@ -10,9 +10,9 @@ import (
 
 const (
 	retryInterval        = time.Second * 5
-	timeout              = time.Second * 120
+	timeout              = time.Second * 300
 	cleanupRetryInterval = time.Second * 1
-	cleanupTimeout       = time.Second * 10
+	cleanupTimeout       = time.Second * 60
 	operatorName         = "kubevirt-image-service"
 )
 

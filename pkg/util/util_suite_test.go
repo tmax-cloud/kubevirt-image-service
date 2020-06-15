819a1251366b788c5db1f993e31da3a44f69abce
woohhan <woohhan@gmail.com>
2020-06-15 15:44:51 +0900

    Implement generic condition array utils

---
+++ pkg/util/util_suite_test.go
@@ -0,0 +1,18 @@
+package util
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/operator-framework/operator-sdk/pkg/log/zap"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"testing"
+)
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.LoggerTo(GinkgoWriter))
+})
+
+func TestVirtualMachineImage(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Util Suite")
+}

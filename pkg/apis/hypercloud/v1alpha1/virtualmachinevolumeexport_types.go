c644e50aedd97bdaf26d416477a5cc6ff42df88d
hyoung-90 <ssarang520@gmail.com>
2020-06-16 17:25:47 +0900

    delete not needed comments

--- pkg/apis/hypercloud/v1alpha1/virtualmachinevolumeexport_types.go
+++ pkg/apis/hypercloud/v1alpha1/virtualmachinevolumeexport_types.go
@@ -4,9 +4,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // VirtualMachineVolumeExportSpec defines the desired state of VirtualMachineVolumeExport
 type VirtualMachineVolumeExportSpec struct {
 	VirtualMachineVolume VirtualMachineVolumeSource            `json:"virtualMachineVolume"`
